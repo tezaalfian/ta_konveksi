d6eba33ef1e91cd625758bb37557be84fca41968
Teza Alfian <tezaalfian2916@gmail.com>
2019-05-26 06:32:54 +0700

    view

---
+++ application/views/admin/contoh_view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contoh</title>
+    <?php $this->load->view('partial/admin/css');?>
+</head>
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+<!-- HEADER -->
+    <?php $this->load->view('partial/admin/header');?>
+
+<!-- SIDEBAR -->
+    <?php $this->load->view('partial/admin/sidebar');?>
+
+<!-- CONTENT -->
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+        <h1>
+            Dashboard
+            <small>Control panel</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+        </section>
+
+        <!-- Main content -->
+
+        <!-- /.content -->
+    </div>
+
+<!-- END CONTENT -->
+
+<!-- FOOTER -->
+    <?php $this->load->view('partial/admin/footer');?>
+</div>
+<!-- LIBRARY JS -->
+    <?php $this->load->view('partial/admin/js');?>
+    
+</body>
+</html>
